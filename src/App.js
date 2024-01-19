a04aead354744f10e7908ff11c79a66024caada3
Kimani-Dominic <muiruridominic001@gmail.com>
2024-01-19 12:40:26 +0300

    Day 4, 5 & 6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
 // src/App.js
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css'
 import Clock from './clock';
 import Greeting from './greetings'
+import EventHandler from './EventHandler';
+
 
 function App() {
+  //state Variables
+const [count, setCount] = useState(0);
+//day 6
+const items = ['Apple', 'Banana', 'Orange', 'Mango'];
+
+useEffect(() => {
+  const timer = setInterval(() => {
+    setCount(prevCount => prevCount + 1);
+  }, 1000);
+
+  return () => clearInterval(timer)
+}, []);
+//
+const [showMessage, setshowMessage] =useState(false);
+
+
+  
   return (
     <div>
       <h1>Welcome to day 3 & 4 of react learning with Skill-captain</h1>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>
+  Increment
+</button>
+<button onClick={() => setshowMessage(showMessage)}>toggle message</button>
+ {showMessage(
+        <p>Hello, React Event Handling!</p>
+      )}
+
+
+
+
+
+
       <Clock />
       <Greeting name="Kimani" message="Today I've learnt on creating functional components and passing data through props" />
-      
+      <EventHandler />
+
+
+
+     <ul>
+      {items.map((item, index) => (
+      <li key={index}>{item}</li>
+      ))}
+     </ul>
     </div>
   );
 }
---
+++ src/EventHandler.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+function EventHandler() {
+  
+const handleClick = () => {
+    console.log('Clicked!');
+  };
+
+  return (
+    <button onClick={handleClick}>
+      Click me to see the event object in browser console
+    </button>
+  );
+}
+
+export default EventHandler;
